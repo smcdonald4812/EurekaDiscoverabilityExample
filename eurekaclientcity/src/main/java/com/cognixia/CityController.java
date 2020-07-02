50d750dd1a36ce927a94efc35cdf533babd46827
smcdonald4812 <36778056+smcdonald4812@users.noreply.github.com>
2020-07-02 11:10:56 -0400

    Add files via upload
    changes

--- eurekaclientcity/src/main/java/com/cognixia/CityController.java
+++ eurekaclientcity/src/main/java/com/cognixia/CityController.java
@@ -2,9 +2,11 @@ package com.cognixia;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,27 +15,20 @@ public class CityController {
 	
 	@Autowired
 	CityUtil cs;
-
-	@GetMapping("/cities")
-	public String getCountries() {
+	
+	@GetMapping("/{country}")
+	public String getCities(@PathVariable String country) {
+		List<String> list = new Vector<>();
+		String l = new String("");
 		Map<String, List<String>> cities = cs.getList();
-		StringBuilder sb = new StringBuilder("<table border=1>");
-		sb.append("<tr><td>USA<td>");
-		cities.get("USA").forEach(x -> sb.append("<td>"+x+"</td>"));
-		sb.append("</tr>");
-		sb.append("<tr><td>China<td>");
-		cities.get("China").forEach(x -> sb.append("<td>"+x+"</td>"));
-		sb.append("</tr>");
-		sb.append("<tr><td>India<td>");
-		cities.get("India").forEach(x -> sb.append("<td>"+x+"</td>"));
-		sb.append("</tr>");
-		sb.append("<tr><td>Pakastan<td>");
-		cities.get("Pakistan").forEach(x -> sb.append("<td>"+x+"</td>"));
-		sb.append("</tr>");
-		sb.append("<tr><td>Canada<td>");
-		cities.get("Canada").forEach(x -> sb.append("<td>"+x+"</td>"));
-		sb.append("</tr>");
-		sb.append("</table>");
-		return sb.toString();
+		if(cities.get(country)!=null) {
+			list = cities.get(country);
+			for(String city: list) l += city+" ";
+		}
+		else {
+			list = cities.get("USA");
+			for(String city: list) l += city+" ";
+		}
+		return l;
 	}
 }
