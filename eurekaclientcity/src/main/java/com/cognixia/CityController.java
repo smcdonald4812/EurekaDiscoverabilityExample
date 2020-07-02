39d23e9032b61adb6199a9a0d291255d5d6a6744
smcdonald4812 <36778056+smcdonald4812@users.noreply.github.com>
2020-07-02 17:40:33 -0400

    Update CityController.java
    dan's default

--- eurekaclientcity/src/main/java/com/cognixia/CityController.java
+++ eurekaclientcity/src/main/java/com/cognixia/CityController.java
@@ -15,19 +15,13 @@ public class CityController {
 	
 	@Autowired
 	CityUtil cs;
-	private boolean flag = true;
 	
 	@GetMapping("/{country}")
 	public String getCities(@PathVariable String country) {
 		List<String> list = new Vector<>();
 		String l = new String("<table border=1>");
 		Map<String, List<String>> cities = cs.getList();
-		if(flag) {
-			flag = false;
-			list = cities.get("USA");
-			for(String city: list) l += "<tr><td>"+city+"</td></tr>";
-			l+=new String("</table>");
-		} else if(cities.get(country)!=null) {
+		if(cities.get(country)!=null) {
 			list = cities.get(country);
 			for(String city: list) l += "<tr><td>"+city+"</td></tr>";
 			l+=new String("</table>");
